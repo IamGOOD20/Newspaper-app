22ec654921c33617d547bd25a29ad3d272970df8
IamGOOD <e.rukshenas@gmail.com>
2023-12-20 22:00:49 -0500

    Configure extended user model

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Newspaper-app
 Django project; Implementation of a custom User model what many Django experts recommend.
+
+Creating our custom user model requires four steps:
+
+• update config/settings.py
+
+• create a new CustomUser model
+
+• create new forms for UserCreationForm and UserChangeForm
+
+• update accounts/admin.p
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
