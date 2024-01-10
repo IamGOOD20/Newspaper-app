2e1858222d1217c7f5bf6e0d440fad7c9bab1bc0
IamGOOD <e.rukshenas@gmail.com>
2024-01-09 22:18:40 -0500

    Used Django build- in Mixins for implementation prohibition to update and delete functionality, also for creation articlws only for log in userUsed Django build- in Mixins for implementation prohibition to update and delete articles,s

--- articles/views.py
+++ articles/views.py
@@ -1,26 +1,48 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+)
+from django.views.generic import (
+    ListView,
+    DetailView)
+
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Artiicle
 from django.urls import reverse_lazy
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Artiicle
     template_name = 'article_list.html'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+    UpdateView
+):
     model = Artiicle
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
-    # success_url = reverse_lazy('article_list')
-class ArticleDeleteView(DeleteView):
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(
+    UserPassesTestMixin,
+    LoginRequiredMixin,
+    DeleteView
+):
     model = Artiicle
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleDetailView(DetailView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Artiicle
     template_name = 'article_detail.html'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Artiicle
     fields = ('title', 'body',)
     template_name = 'article_new.html'
